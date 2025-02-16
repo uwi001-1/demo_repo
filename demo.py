9132ad603d8363568979170c262f00537e5b7996
uwi001_1 <022a106@sxc.edu.np>
2025-02-16 20:06:00 +0545

    Assignment Day-5

--- demo.py
+++ demo.py
@@ -57,3 +57,12 @@ if s == s[::-1]:
     print("Palindrome")
 else:
     print("Not a Palindrome")
+
+
+def isPalindrome(s):
+    rev = ''.join(reversed(s))
+    if (s == rev):
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")
+ans = isPalindrome("dad")
